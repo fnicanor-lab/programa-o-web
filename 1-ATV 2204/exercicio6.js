f13c9a2020d040e5f9d1ddf173cfa62e680aa67f
felipe <f.nicanor@estudante.ifmt.edu.br>
2026-04-29 21:55:28 -0400

    primeiro commit

---
+++ 1-ATV 2204/exercicio6.js
@@ -0,0 +1,15 @@
+let nivel = 3;
+
+switch (nivel) {
+    case 1:
+        console.log("Iniciante");
+        break;
+    case 2:
+        console.log("Intermediário");
+        break;
+    case 3:
+        console.log("Avançado");
+        break;
+    default:
+        console.log("Número inválido");
+}
---
+++ 1-ATV 2204/exercicio7.js
@@ -0,0 +1,5 @@
+let idade = 26
+
+let resultado = idade >= 18 ? "Maior de idade" : "Menor de idade";
+
+console.log(resultado);
